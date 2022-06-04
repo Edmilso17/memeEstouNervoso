cfbceb95f2bcd6660eb7f8f7a5101f719cf83aec
edmilso17 <edmilsonribeiro.17072005@gmail.com>
2022-06-04 17:45:48 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MemeToNervoso
+{
+    class Program
+    {
+        static void Main(string[] args)
+{       
+    string fraseDigitada, fraseMemeEstouNervoso;
+
+    Console.Write("Usuario, o que pensa sobre mim? ");
+    fraseDigitada = Console.ReadLine();
+
+    fraseMemeEstouNervoso = $"{fraseDigitada}...NÃO PERA. \n estou nervoso";
+
+    Console.WriteLine(fraseMemeEstouNervoso);
+}
+    }
+}
